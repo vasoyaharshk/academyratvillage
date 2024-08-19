9e73dbde4610e2e4ca10b7efedb5c0fa74178468
vasoyaharshk <vasoyaharshkumar@gmail.com>
2024-08-19 21:45:36 +0200

    Probability stage 1 working. Gal Luna Sparky in one script now.

--- user/settings.py
+++ user/settings.py
@@ -98,10 +98,10 @@ CAM1_STATES = {}
 CAM1_DURATION_VIDEO = 1800
 CAM1_NUMBER_OF_VIDEOS = 100000
 CAM1_THRESHOLD = 0
-CAM1_CAGE_ZONE1 = [65, 140, 92, 195]       # <-- TO CHANGE left, right, top, down
-CAM1_CAGE_ZONE2 = [65, 238, 195, 300]     # <-- TO CHANGE left, right, top, down
-CAM1_DOORS1_ZONE = [238, 488, 258, 295]   # <-- TO CHANGE left, right, top, down
-CAM1_DOORS2_ZONE = [488, 595, 265, 290]   # <-- TO CHANGE left, right, top, down
+CAM1_CAGE_ZONE1 = [60, 135, 92, 195]       # <-- TO CHANGE left, right, top, down
+CAM1_CAGE_ZONE2 = [60, 233, 195, 300]     # <-- TO CHANGE left, right, top, down
+CAM1_DOORS1_ZONE = [233, 483, 258, 295]   # <-- TO CHANGE left, right, top, down
+CAM1_DOORS2_ZONE = [483, 590, 265, 290]   # <-- TO CHANGE left, right, top, down
 CAM1_TEXT_X = 25                         # <-- TO CHANGE
 CAM1_TEXT_Y = 315                         # <-- TO CHANGE
 
