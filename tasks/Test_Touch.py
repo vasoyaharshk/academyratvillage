e5826f5137151548fafea1f6d792409ff41a977a
vasoyaharshk <vasoyaharshkumar@gmail.com>
2024-10-03 12:00:12 +0200

    Everything checked and working

--- tasks/Test_Touch.py
+++ tasks/Test_Touch.py
@@ -3,53 +3,63 @@ from pybpodapi.protocol import Bpod
 from academy.utils import utils
 from user import settings
 
+
 class Test_Touch(Task):
 
     def __init__(self):
         super().__init__()
 
         self.info = """
+        ########   TASK INFO   ########
+        Mice learn to touch the screen during the response window to obtain the reward.
+        Animals crossing the end of the corridor trigger the stimulus presentation in all holes and response window onset. 
+        Screen touches during response window deliver reward.
+        Stimulus low contrast to not scare animals
+
+        ########   PORTS INFO   ########
+        Port 1 - WATER PORT: LED, photogates and pump
+        Port 2 - BUZZER: valve (16kHz): correct
+        Port 3 - PHOTOGATES 4: Photogates end of corridor
+        Port 4 - PHOTOGATES 0: Photogates next to lickport & Global LED
+
+
+                ########   PORTS INFO   ########
+        Port 1 - WATER PORT: LED, photogates and pump
+        Port 2 - PHOTOGATES 2: Photogates next to lickport 
+        Port 3 - PHOTOGATES 3: Photogates 
+        Port 4 - PHOTOGATES 4: Photogates 
+        Port 5 - PHOTOGATES 5: Photogates 
+        Port 6 - PHOTOGATES 6: Photogates next to screen , global LED
+
         """
 
     def init_variables(self):
         # general
-        self.duration_min = 1800 # 30 mins
-        self.duration_max = 2100 # 40 mins
+        self.duration_min = 2100  # 30 mins   #changed from 30 mins to 2100 for Luna
+        self.duration_max = 3000  # 35 mins   #changed from 35 mins to 3000 for Luna
         self.stage = 0
         self.substage = 0
-        self.response_duration = 20
+        self.response_duration = 120  # 2 min
         self.stim_duration = self.response_duration
-        self.trials_max = 10
 
-        #Screen Details:
-        self.x = 188  # Centered horizontally
-        self.y = 102  # Positioned vertically
-        self.width = 60  # Stimulus width in mm
-        self.height = 60  # Stimulus height in mm
-        self.contrast = 1.2  # Contrast level
-
-        # Make the correct_th area to the size of the rectangles:
-        # Calculate half-width and half-height
-        self.half_width_mm = self.width / 2
-        self.half_height_mm = self.height / 2
-
-        # Calculate the correct_th as the diagonal distance from the center to the corners and make that area as correct:
-        self.correct_th = ((self.half_width_mm ** 2 + self.half_height_mm ** 2) ** 0.5) + 10
-
-        # Repoke threshold (assuming full screen width)
-        self.repoke_th = settings.WIN_SIZE[0] * 2  # Full screen
+        # screen details
+        self.x = [60, 175, 290]  # screen width is 401mmm
+        self.y = 30  # screen height is 250mmm
+        self.width = 80
+        self.correct_th = settings.WIN_SIZE[0] * 2  # full screen
+        self.repoke_th = settings.WIN_SIZE[0] * 2  # full screen
+        self.contrast = 0.4  # 0 black, 1 gray, 2 white. Default 20%
 
         # pumps
         self.valve_time = utils.water_calibration.read_last_value('port', 1).pulse_duration
         self.valve_reward = utils.water_calibration.read_last_value('port', 1).water
-        self.valve_factor_c = 1.5
-        self.valve_factor_i = 0.5
+        self.valve_factor_c = 1.5  # More reward for correct. Changed from 1.5 to 3.0 for Luna only.
+        self.valve_factor_i = 0.5  # Less reward for misses
 
         # counters
         self.reward_drunk = 0
 
-
-    def configure_gui(self): # Variables that appear in the GUI
+    def configure_gui(self):  # Variables that appear in the GUI
         pass
 
     def main_loop(self):
@@ -61,32 +71,38 @@ class Test_Touch(Task):
                 state_name='Start_task',
                 state_timer=0,
                 state_change_conditions={Bpod.Events.Tup: 'Real_start'},
-                output_actions=[(Bpod.OutputChannels.SoftCode, 2)])
-            # show stim inifite time
+                output_actions=[])
 
             self.sma.add_state(
                 state_name='Real_start',
                 state_timer=self.valve_time * 2,
-                state_change_conditions={Bpod.Events.Tup: 'Fixation'},
-                output_actions=[(Bpod.OutputChannels.SoftCode, 20)])
-            # close corridor 2 door, and deliver water when animal enter to behav box
+                state_change_conditions={Bpod.Events.Tup: 'Wait_for_fixation'},
+                output_actions=[(Bpod.OutputChannels.SoftCode, 20), (Bpod.OutputChannels.Valve, 1)])
+            # close corridor door 2 when subject enter to the behav box
+
         else:
             self.sma.add_state(
                 state_name='Start_task',
                 state_timer=0,
-                state_change_conditions={Bpod.Events.Tup: 'Fixation'},
+                state_change_conditions={Bpod.Events.Tup: 'Wait_for_fixation'},
                 output_actions=[])
+
+        self.sma.add_state(
+            state_name='Wait_for_fixation',
+            state_timer=0,
+            state_change_conditions={Bpod.Events.Tup: 'Fixation'},
+            output_actions=[])
 
         self.sma.add_state(
             state_name='Fixation',
             state_timer=0,
             state_change_conditions={Bpod.Events.Tup: 'Response_window'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 2)])
-            # show 3 stimuli when crossing end of corridor
+            output_actions=[(Bpod.OutputChannels.SoftCode, 3)])
+        # show 3 stimuli when crossing end of corridor
 
         self.sma.add_state(
             state_name='Response_window',
-            state_timer=self.response_duration+10,
+            state_timer=self.response_duration + 10,
             state_change_conditions={'SoftCode1': 'Correct_first', 'SoftCode3': 'Miss', Bpod.Events.Tup: 'Miss'},
             output_actions=[(Bpod.OutputChannels.SoftCode, 4)])
 
@@ -94,45 +110,44 @@ class Test_Touch(Task):
             state_name='Correct_first',
             state_timer=1,
             state_change_conditions={Bpod.Events.Tup: 'Correct_first_reward'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 11)])
+            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.SoftCode, 11)])
+        # waterLED and RWsound remain ON until poke
 
         self.sma.add_state(
             state_name='Correct_first_reward',
             state_timer=self.valve_time * self.valve_factor_c,
             state_change_conditions={Bpod.Events.Tup: 'Exit'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 15)])
+            output_actions=[(Bpod.OutputChannels.Valve, 1), (Bpod.OutputChannels.SoftCode, 17)])
 
         self.sma.add_state(
             state_name='Miss',
             state_timer=1,
             state_change_conditions={Bpod.Events.Tup: 'Miss_reward'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 12)])
+            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.LED, 6),
+                            (Bpod.OutputChannels.SoftCode, 12)])
+        # waterLED ON, global LED ON
 
         self.sma.add_state(
             state_name='Miss_reward',
             state_timer=self.valve_time * self.valve_factor_i,
             state_change_conditions={Bpod.Events.Tup: 'Exit'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 15)])
+            output_actions=[(Bpod.OutputChannels.Valve, 1), (Bpod.OutputChannels.SoftCode, 17)])
 
         self.sma.add_state(
             state_name='Exit',
-            state_timer=3,
+            state_timer=0,
             state_change_conditions={Bpod.Events.Tup: 'exit'},
             output_actions=[])
-
 
     def after_trial(self):
 
         # Trial Counter
-        if self.current_trial_states['Miss'][0][0] > 0: # Missed trial
+        if self.current_trial_states['Miss'][0][0] > 0:  # Missed trial
             self.register_value('trial_result', 'miss')
             self.reward_drunk += self.valve_reward * self.valve_factor_i
-            print("_______ MISSED ________")
         else:
-            self.register_value('trial_result', 'correct_first') # Correct trial
+            self.register_value('trial_result', 'correct_first')  # Correct trial
             self.reward_drunk += self.valve_reward * self.valve_factor_c
-
-        print('Resp coord:' + str(self.response_x))
 
         # Relevant prints
         self.register_value('reward_drunk', self.reward_drunk)
