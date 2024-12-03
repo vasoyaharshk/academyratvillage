d8085968bb9ca772b8d8c021c56c79eeba643177
vasoyaharshk <vasoyaharshkumar@gmail.com>
2024-12-03 18:08:26 +0100

    Weber's Law Implemented

--- user/settings.py
+++ user/settings.py
@@ -102,8 +102,8 @@ CAM1_NUMBER_OF_VIDEOS = 100000
 CAM1_THRESHOLD = 0
 CAM1_CAGE_ZONE1 = [60, 135, 92, 195]       # <-- TO CHANGE left, right, top, down
 CAM1_CAGE_ZONE2 = [60, 227, 195, 300]     # <-- TO CHANGE left, right, top, down
-CAM1_DOORS1_ZONE = [227, 476, 260, 295]   # <-- TO CHANGE left, right, top, down Harsh done
-CAM1_DOORS2_ZONE = [476, 584, 265, 290]   # <-- TO CHANGE left, right, top, down
+CAM1_DOORS1_ZONE = [227, 473, 260, 295]   # <-- TO CHANGE left, right, top, down Harsh done
+CAM1_DOORS2_ZONE = [473, 584, 265, 290]   # <-- TO CHANGE left, right, top, down
 CAM1_TEXT_X = 25                         # <-- TO CHANGE
 CAM1_TEXT_Y = 315                         # <-- TO CHANGE
 
