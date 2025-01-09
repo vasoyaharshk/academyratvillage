1f29e76fed67d8735a6cf5f1f07d9134795da568
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-01-09 10:48:09 +0100

    Weber's law images changed

--- tasks/Probability_Training_BB_Demotivation.py
+++ tasks/Probability_Training_BB_Demotivation.py
@@ -13,8 +13,8 @@ class Probability_Training_BB_Demotivation(Task):
         This task displays the image of the jars which are touchable. This script includes repoketh, the ability to make correct choices.
         ########   TASK INFO   ########
         Stage 1: Indication: Only blue jar of pegs stimulus appears Blue is rewarding and yellow unrewarding
-        Stage 2: Discrimination 1: Blue and yellow jar of pegs appears (100% each)
-        Stage 3: Discrimination 2: Blue and yellow jar of pegs appears (1 jar is 100% of unrewarded color yellow and the other is 50%)
+        Stage 2: Discrimination a: Blue and yellow jar of pegs appears (100% each)
+        Stage 3: Discrimination b: Blue and yellow jar of pegs appears (1 jar is 100% of unrewarded color yellow and the other is 50%)
 
                 ########   PORTS INFO   ########
         Port 1 - WATER PORT: LED, photogates and pump
