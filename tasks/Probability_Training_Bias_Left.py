08044cf8fb5f48446d488146a7ac40ca9561a273
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-01-10 16:29:41 +0100

    Weber's law images changed

--- tasks/Probability_Training_Bias_Left.py
+++ tasks/Probability_Training_Bias_Left.py
@@ -10,7 +10,7 @@ class Probability_Training_Bias_Left(Task):
         super().__init__()
 
         self.info = """
-        This script is IF THE BIAS IS TOWARDS THE LEFT SIDE.
+        This script is IF THE BIAS IS TOWARDS THE LEFT SIDE.  So the stim will be displayed on the RIGHT more than the LEFT.
         This task displays the image of the jars which are touchable.
         ########   TASK INFO   ########
         Stage 1: Indication: Only blue jar of pegs stimulus appears Blue is rewarding and yellow unrewarding
@@ -105,9 +105,9 @@ class Probability_Training_Bias_Left(Task):
     def generate_random_trials(self, last_trial=None):  # Generates a series of stim outputs where none are repeated more than 2 times in sequence.
         trials = []
         if self.substage == 1:
-            self.probabilities = [0.9, 0.1]
+            self.probabilities = [0.1, 0.9]
         elif self.substage == 2:
-            self.probabilities = [0.75, 0.25]
+            self.probabilities = [0.25, 0.75]
           # Adjust this if you have more than two stimuli
         while len(trials) < 1000:
             # Use random.choices to select a candidate with 50% probability for each stimulus
