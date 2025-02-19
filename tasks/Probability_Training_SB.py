274c549d89295b527fe5912137bbe2dd36cf4942
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-02-19 21:21:05 +0100

    Image path gets saved in WL training and Turtle

--- tasks/Probability_Training_SB.py
+++ tasks/Probability_Training_SB.py
@@ -13,7 +13,7 @@ class Probability_Training_BB_Joy(Task):
 
         self.info = """
         This task displays the image of the jars which are touchable. This script is the main script now with side bias breaking.
-        This script is only for Joy where stage 2 and stage 3 are big jars only
+        This script is where the jars are a mix of small and big.
         ########   TASK INFO   ########
         Stage 1: Indication: Only blue jar of pegs stimulus appears Blue is rewarding and yellow unrewarding
         Stage 2: Discrimination a: Blue and yellow jar of pegs appears (100% each). Both are big jars.
