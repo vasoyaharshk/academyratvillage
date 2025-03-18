83973dec16d2ecdbc3e48c0ddea5a613b7fd50a1
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-03-18 12:35:53 +0100

    Adding Webers Law 40 trial rule

--- user/select_task.py
+++ user/select_task.py
@@ -63,6 +63,9 @@ def select_task(df, subject):
 
     my_subject = df.subject.iloc[0]
 
+    #Remove all the blank trials:
+    #df = df.loc[~((df['trial_length'] == 0.1) & (df['trial_result'].isna()))].copy()
+
     # Check if task does not contain the word 'Probability'
     if 'Probability' not in task:  #Excludes all the task without the word Probability
         # dataframes
