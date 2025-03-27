07d1704059145216f30c3873fe7dd7a32da309c4
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-03-27 12:26:24 +0100

    Added Extra Training 40 trial rule + resolved misses being counted

--- user/select_task.py
+++ user/select_task.py
@@ -91,11 +91,35 @@ def select_task(df, subject):
                     pass
 
             if task_number == 2:
-                stim_trial = 0
-                stim_trials = []
-                stim_trial_counter = 0
-                block_number = 0
+                stage = 1  # Current stage within the task
+                substage = 0  # Current substage within the stage
+                substage_bias = 0  # Side bias stage for substage behavior
+                task_number = 2  # Each task has a unique number. See RV script guide.
 
+                # Needed to create blocks of 40 trials for criterion to be assessed on:
+                block_size = 4  # The number of trials in a block
+                block_trial_counter = 0  # Trial count within the current block
+                block_accuracy = 0.0  # Accuracy in the current block
+                block_number = 0  # Sequential block number
+                ror_change = 0  # If it is 1, ROR will change on the next trial.
+                block_change = 0  # If it is 1, a new block will start on the next trial
+                total_trials = 0  # Total trials across the task.
+                block_correct_count = 0  # Number of correct responses in the block
+                block_valid_count = 0  # Number of valid (non-missed) trials in the block
+                condition_trial_counter = 0  # Counter for randomising conditions
+                last_forward_stage = 0  # The stage moved forward from after a forward change
+                last_backward_stage = 0  # The stage moved backward to after the last backward change
+                moved_back_counter = 0  # Counter for how many times the subject moved back a stage
+                stage_forward_change = 0  # Whether stage move forward on the next trial
+                stage_backward_change = 0  # Whether stage move backward on the next trial
+
+                # Left Right Function Randomisation variables:
+                stim_trial = 0  # The function number of the correct stimulus in the current trial. This designates trial type, e.g. from Discrim. C: left is correct, big jar is correct, spacer in correct
+                stim_trials = []  # List of correct stimulus function randomised.
+                stim_trial_counter = 0  # It counts the number of trials within a randomization block. Doesnt change when Bias breaking is active.
+                last_stim_trial = 0  # the function of the last trial of the previous block. Used to ensure first trial of next block is different
+
+                task = 'Probability_Training_BB_Size_Acc'
                 message = 'Advance from Etra training to Core training'
                 try:
                     telegram_bot.alarm_finish_session(message, my_subject)
@@ -103,10 +127,8 @@ def select_task(df, subject):
                 except:
                     print('Telegram message not sent')
                     pass
-                task = 'Probability_Training_BB_Size'
-                stage = 1
 
-        if 'Probability_Training_BB_Size' in task:
+        if 'Probability_Training_BB_Size_Acc' in task:
             if moved_back_counter > max_move_backs:
                 message = f"URGENT: Moved back {moved_back_counter} FOR {my_subject}. CHECK DATA."
                 try:
@@ -116,7 +138,29 @@ def select_task(df, subject):
                     pass
 
             if task_number == 3:
-                block_number = 0
+                substage = 0  # Current substage within the stage
+                substage_bias = 0  # Side bias stage for substage behavior
+                task_number = 2  # Each task has a unique number. See RV script guide.
+
+                # Needed to create blocks of 40 trials for criterion to be assessed on:
+                block_size = 4  # The number of trials in a block
+                block_trial_counter = 0  # Trial count within the current block
+                block_accuracy = 0.0  # Accuracy in the current block
+                block_number = 0  # Sequential block number
+                ror_change = 0  # If it is 1, ROR will change on the next trial.
+                block_change = 0  # If it is 1, a new block will start on the next trial
+                total_trials = 0  # Total trials across the task.
+                block_correct_count = 0  # Number of correct responses in the block
+                block_valid_count = 0  # Number of valid (non-missed) trials in the block
+                condition_trial_counter = 0  # Counter for randomising conditions
+                last_forward_stage = 0  # The stage moved forward from after a forward change
+                last_backward_stage = 0  # The stage moved backward to after the last backward change
+                moved_back_counter = 0  # Counter for how many times the subject moved back a stage
+                stage_forward_change = 0  # Whether stage move forward on the next trial
+                stage_backward_change = 0  # Whether stage move backward on the next trial
+
+                # Left Right Function Randomisation variables:
+                last_stim_trial = 0  # the function of the last trial of the previous block. Used to ensure first trial of next block is different
 
                 message = 'PI: Advance from Core training to Webers Law Pre Test'
                 try:
