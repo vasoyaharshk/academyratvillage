da1b612e1132f86dfd04eea11d4b3dac5ff9ea33
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-04-02 12:58:28 +0200

    New touchteaching for general training

--- tasks/TouchTeaching_no_mask_V1.py
+++ tasks/TouchTeaching_no_mask_V1.py
@@ -6,7 +6,8 @@ from academy.utils import utils
 from user import settings
 import random
 
-class TouchTeaching(Task):
+
+class TouchTeaching_no_mask_V1(Task):
 
     def __init__(self):
         super().__init__()
@@ -17,7 +18,7 @@ class TouchTeaching(Task):
         Animals crossing the end of the corridor trigger the stimulus presentation in all holes and response window onset. 
         Screen touches during response window deliver reward.
         Stimulus low contrast to not scare animals
-              
+
                 ########   PORTS INFO   ########
         Port 1 - WATER PORT: LED, photogates and pump
         Port 2 - PHOTOGATES 2: Photogates next to lickport 
@@ -25,16 +26,16 @@ class TouchTeaching(Task):
         Port 4 - PHOTOGATES 4: Photogates 
         Port 5 - PHOTOGATES 5: Photogates 
         Port 6 - PHOTOGATES 6: Photogates next to screen , global LED
-        
+
         """
 
     def init_variables(self):
         # general
-        self.duration_min = 1800 # 30 mins   # minimum session duration
-        self.duration_max = 2100 # 35 mins   # max
+        self.duration_min = 1800  # 30 mins   # minimum session duration
+        self.duration_max = 2100  # 35 mins   # max
         self.stage = 0
         self.substage = 0
-        self.response_duration = 120 # 2 min
+        self.response_duration = 120  # 2 min
         self.stim_duration = self.response_duration
 
         # screen details
@@ -43,21 +44,22 @@ class TouchTeaching(Task):
         self.width = 0
         self.height = 0
         self.x_correcth = settings.WIN_SIZE[0] * 2  # can touch anywhere when it's a normal trial
-        self.contrast= 0.4 # contrast of the stim. 0 black, 1 gray, 2 white. Default 40%
+        self.x_incorrecth = 0  # Incorrecth coordinate
+        self.contrast = 0.4  # contrast of the stim. 0 black, 1 gray, 2 white. Default 40%
 
         # pumps
         self.valve_time = utils.water_calibration.read_last_value('port', 1).pulse_duration
         self.valve_reward = utils.water_calibration.read_last_value('port', 1).water
-        self.valve_factor_c = 2.0 #More reward for correct. 2.0 = 50uL
-        #self.valve_factor_i = 0.5 #Less reward for misses
+        self.valve_factor_c = 2.0  # More reward for correct. 2.0 = 50uL
+        # self.valve_factor_i = 0.5 #Less reward for misses
 
         # counters
-        self.reward_drunk = 0 # how many uLs they have received in the session
+        self.reward_drunk = 0  # how many uLs they have received in the session
 
         # Needed in Each Task:
         self.stage = 0  # Current stage within the task
         self.substage = 0  # Current substage within the stage
-        #self.task_number = 0  # Each task has a unique number. See RV script guide.
+        # self.task_number = 0  # Each task has a unique number. See RV script guide.
 
         # Needed to create blocks of 40 trials for criterion to be assessed on:
         self.block_size = 0  # The number of trials in a block
@@ -81,9 +83,7 @@ class TouchTeaching(Task):
         self.last_stim_trial = 0  # the function of the last trial of the previous block. Used to ensure first trial of next block is different
         self.stim = [0]
 
-
-
-    def configure_gui(self): # Variables that appear in the GUI
+    def configure_gui(self):  # Variables that appear in the GUI
         pass
 
     def main_loop(self):
@@ -97,17 +97,18 @@ class TouchTeaching(Task):
             self.width = settings.WIN_SIZE[0] * 2
         elif self.stage == 2:
             self.stim = 202
-            self.x_correcth = random.randint(65, 325) # Screen width is 401mmm
+            self.x_correcth = random.randint(65, 325)  # Screen width is 401mmm
             self.y_correcth = 110  # 640 = center of the screen. Screen height is 250mmm
             self.width = 125
             self.height = 125
         elif self.stage == 3:
             self.stim = 203
-            self.x_correcth = random.randint(33, 357)  # Screen width is 410mmm. We minus 10 on each end to account for mask
-            self.y_correcth = random.randint(43, 82)  # 640 = center of the screen. Screen height is 250mmm. We minus 10 on each end to account for mask
+            self.x_correcth = random.randint(33,
+                                             357)  # Screen width is 410mmm. We minus 10 on each end to account for mask
+            self.y_correcth = random.randint(43,
+                                             82)  # 640 = center of the screen. Screen height is 250mmm. We minus 10 on each end to account for mask
             self.width = 65
             self.height = 65
-
 
         if self.current_trial == 0:
             self.sma.add_state(
@@ -121,109 +122,62 @@ class TouchTeaching(Task):
                 state_timer=self.valve_time * 2,
                 state_change_conditions={Bpod.Events.Tup: 'Wait_for_fixation'},
                 output_actions=[(Bpod.OutputChannels.SoftCode, 20), (Bpod.OutputChannels.Valve, 1)])
-                # close corridor door 2 when subject enter to the behav box
+            # close corridor door 2 when subject enter to the behav box
 
         else:
             self.sma.add_state(
                 state_name='Start_task',
                 state_timer=0,
                 state_change_conditions={Bpod.Events.Tup: 'Wait_for_fixation'},
-                output_actions=[]) # because there's nothing in the brackets, this does nothing
+                output_actions=[])  # because there's nothing in the brackets, this does nothing
 
         self.sma.add_state(
             state_name='Wait_for_fixation',
             state_timer=0,
-            state_change_conditions={Bpod.Events.Port6In: 'Stimulus_Display'}, # when the last photogate is crossed, the stimulus is displayed
+            state_change_conditions={Bpod.Events.Port6In: 'Stimulus_Display'},
+            # when the last photogate is crossed, the stimulus is displayed
             output_actions=[])
 
         self.sma.add_state(
-            state_name='Stimulus_Display', #
+            state_name='Stimulus_Display',  #
             state_timer=0,
-            state_change_conditions={Bpod.Events.Tup: 'Response_window'}, # starts the response window
-            output_actions=[(Bpod.OutputChannels.SoftCode, self.stim)]) #
-            # show 3 stimuli when crossing end of corridor
+            state_change_conditions={Bpod.Events.Tup: 'Response_window'},  # starts the response window
+            output_actions=[(Bpod.OutputChannels.SoftCode, self.stim)])  #
+        # show 3 stimuli when crossing end of corridor
 
         self.sma.add_state(
             state_name='Response_window',
-            state_timer=self.response_duration,
-            state_change_conditions={'SoftCode1': 'Correct', 'SoftCode3': 'Touch_Outside', 'SoftCode4': 'Punish',
-                                     Bpod.Events.Tup: 'No_Touch'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 34)])
-        # Starts to read the touchscreen with one touch processing
+            state_timer=self.response_duration + 10,
+            state_change_conditions={'SoftCode1': 'Correct_first', 'SoftCode3': 'Miss', Bpod.Events.Tup: 'Miss'},
+            output_actions=[
+                (Bpod.OutputChannels.SoftCode, 204)])  # function 204 defines the active touch area of our stims
 
         self.sma.add_state(
-            state_name='Correct',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Tup: 'Correct_image_display'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.SoftCode, 38)])
-        # Turns on Water port LED and plays correct sound
+            state_name='Correct_first',
+            state_timer=1,
+            state_change_conditions={Bpod.Events.Port1In: 'Correct_first_reward'},
+            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.SoftCode, 11)])
+        # waterLED and RWsound remain ON until poke
 
         self.sma.add_state(
-            state_name='Correct_image_display',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Port1In: 'Correct_reward', Bpod.Events.Tup: 'Flip_screen_reward'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.SoftCode, 63)])
-        # Turns on Water port LED and plays correct sound and displays correct stimuli for image_display (3 seconds)
-
-        self.sma.add_state(
-            state_name='Correct_reward',
+            state_name='Correct_first_reward',
             state_timer=self.valve_time * self.valve_factor_c,
             state_change_conditions={Bpod.Events.Tup: 'Exit'},
             output_actions=[(Bpod.OutputChannels.Valve, 1), (Bpod.OutputChannels.SoftCode, 17)])
-        # Delivers Water and stops the reward sound and flips the screen
 
         self.sma.add_state(
-            state_name='Flip_screen_reward',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Port1In: 'Correct_reward'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.SoftCode, 40)])
-        # Turns on Water port LED and plays correct sound and flips screen after 3 seconds
-
-        self.sma.add_state(
-            state_name='Touch_Outside',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Tup: 'Response_window'},
-            output_actions=[])
-        # Goes back to response window in case of touch outside the two jar areas
-
-        self.sma.add_state(
-            state_name='Punish',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Tup: 'Punish_image_display'},
+            state_name='Miss',
+            state_timer=1,
+            state_change_conditions={Bpod.Events.Port1In: 'Miss_reward'},
             output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.LED, 6),
-                            (Bpod.OutputChannels.SoftCode, 39)])
-        # Turns on Global LED and water port LED on
+                            (Bpod.OutputChannels.SoftCode, 12)])
+        # waterLED ON, global LED ON
 
         self.sma.add_state(
-            state_name='Punish_image_display',
-            state_timer=self.image_display,
-            state_change_conditions={Bpod.Events.Port1In: 'After_punish', Bpod.Events.Tup: 'Flip_screen_no_reward'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.LED, 6),
-                            (Bpod.OutputChannels.SoftCode, 64)])
-        # Turns on Global LED and water port LED on, and displays incorrect stimuli for image_display (3 seconds) nad plays punish sound for 1 second.
-
-        self.sma.add_state(
-            state_name='After_punish',
-            state_timer=0,
+            state_name='Miss_reward',
+            state_timer=self.valve_time * self.valve_factor_i,
             state_change_conditions={Bpod.Events.Tup: 'Exit'},
-            output_actions=[(Bpod.OutputChannels.SoftCode, 40)])
-        # Flips the screen after water port poked in.
-
-        self.sma.add_state(
-            state_name='Flip_screen_no_reward',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Port1In: 'Exit'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.LED, 6),
-                            (Bpod.OutputChannels.SoftCode, 40)])
-        # Turns on Water port LED and plays correct sound and flips screen after 3 seconds
-
-        self.sma.add_state(
-            state_name='No_Touch',
-            state_timer=0,
-            state_change_conditions={Bpod.Events.Port1In: 'Exit', Bpod.Events.Port2In: 'Exit'},
-            output_actions=[(Bpod.OutputChannels.PWM1, 5), (Bpod.OutputChannels.LED, 6),
-                            (Bpod.OutputChannels.SoftCode, 37)])
-        # Turns on Water port LED and Global LED and displays message on camera for miss and flips the screen to displays blank,
+            output_actions=[(Bpod.OutputChannels.Valve, 1), (Bpod.OutputChannels.SoftCode, 17)])
 
         self.sma.add_state(
             state_name='Exit',
@@ -231,15 +185,14 @@ class TouchTeaching(Task):
             state_change_conditions={Bpod.Events.Tup: 'exit'},
             output_actions=[])
 
-
     def after_trial(self):
 
         # Trial Counter
-        if self.current_trial_states['Miss'][0][0] > 0: # Missed trial
+        if self.current_trial_states['Miss'][0][0] > 0:  # Missed trial
             self.register_value('trial_result', 'miss')
             self.reward_drunk += self.valve_reward * self.valve_factor_i
         else:
-            self.register_value('trial_result', 'correct_first') # Correct trial
+            self.register_value('trial_result', 'correct_first')  # Correct trial
             self.reward_drunk += self.valve_reward * self.valve_factor_c
 
         # Relevant prints
