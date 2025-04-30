dc2f735d55bd4b761159410ea29ef8e0d393387f
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-04-30 14:35:39 +0200

    Added Extra Training 40 trial rule + resolved misses being counted

--- academy/__main__.py
+++ academy/__main__.py
@@ -747,7 +747,7 @@ def go_to_state(num):
             utils.subject_trapped = False
             arduino.close_door1()
         arduino.open_door2()
-        utils.log_cam(utils.subject.name, "XXXXX. Opening door2, subject can leave", "ACTION")
+        utils.log_cam(utils.subject.name, "Opening door2, subject can leave", "ACTION")
         utils.log("Academy", "Go to state 3", "ACTION")
 
     elif num == 4:  # after min_time, data not saved, animal back
