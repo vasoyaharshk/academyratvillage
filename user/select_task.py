80d924c2bbd0add67001c60d04e6f8bd922d855c
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-05-29 17:17:22 +0200

    Weighing scale disabled and enabled versions

--- user/select_task.py
+++ user/select_task.py
@@ -19,9 +19,6 @@ def select_task(df, subject):
     last_row = df.iloc[-1]  # Get the last row of the DataFrame
     my_subject = df.subject.iloc[0]
 
-    if my_subject == 'm3':
-        wait_seconds = 1
-
     #Assign reward decibels: Needs to be a dictionary if different for each individual.
     reward_db = 70.0
 
@@ -622,6 +619,9 @@ def select_task(df, subject):
 
     # Remove all the blank trials: It doesnt work as the file doesn'd get saved here.
     df = df.loc[~((df['trial_length'] == 0.1) & (df['trial_result'].isna()))].copy()
+
+    if my_subject == 'm3':
+        wait_seconds = 1
 
     #all of these are written in subjects.csv:
     return task, stage, substage, substage_bias, wait_seconds, stim_dur_ds, stim_dur_dm, stim_dur_dl, choice, block, conditions, completed_conditions, current_condition, repetition, current_repetition, trial_counter, stim_trial, stim_trials, stim_trial_counter, ror, completed_ror, current_ror, trial_counter_ror, moved_back_counter, block_size, block_trial_counter, block_accuracy, block_number, ror_change, block_change, last_stim_trial, last_condition_trial, total_trials, block_correct_count, block_valid_count, condition_trial_counter,stage_forward_change,stage_backward_change, task_number, last_forward_stage, last_backward_stage, reward_frequency, reward_db, reward_duration
