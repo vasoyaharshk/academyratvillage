9dc6ffbbd6019cb60df00424dfa0a08a677e37db
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-09-17 17:35:57 +0200

    Reward increased to 75

--- user/select_task.py
+++ user/select_task.py
@@ -368,7 +368,7 @@ def select_task(df, subject):
                     pass
 
                 stage = 4
-                task = 'Probability_WebersLaw_Pre'
+                task = 'abc'
                 block = 12  # This is the number of trials one conditions will remain for
                 conditions = []  # Takes the conditions from select task file.
                 completed_conditions = []  # To store completed conditions
