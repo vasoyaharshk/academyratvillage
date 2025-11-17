bd8be74c9d79cfca4a203f3ac3256f83bbf004da
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-11-17 18:24:39 +0100

    Reward decreased by 50% in all tasks

--- user/functions.py
+++ user/functions.py
@@ -1965,8 +1965,8 @@ def function204(): #Touchteaching read touchscreen
     py = settings.PIXELS_PER_MM_Y
 
     print("Stage= ", stage)
-    width = (utils.task.width + 50) * px
-    height = (utils.task.height + 75) * py
+    width = (utils.task.width + 0) * px
+    height = (utils.task.height + 0) * py
     x_correct = utils.task.x_correcth * px
     # x_incorrect = utils.task.x_incorrecth * px
     y = utils.task.y_correcth * py
