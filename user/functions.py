7335d85885ce3d5f2734002a77e203ccb26ffd19
vasoyaharshk <vasoyaharshkumar@gmail.com>
2025-11-18 15:49:49 +0100

    Reward decreased by 50% in all tasks

--- user/functions.py
+++ user/functions.py
@@ -1025,22 +1025,7 @@ def loop56(timing):
         window.flip()
 
 def function57(): #For A_TouchTeaching_incorrect, punish doenst display image
-    global last_function_called
-
-    rat = utils.task.subject.lower()
-
-    if rat in ['m2', 'ciri', 'gal', 'joy', 'luna', 'sorrel', 'sparky']:
-        soundStream.play(soundVec3)
-    elif rat in ['monika', 'phoebe', 'rachel']:
-        soundStream.play(soundVec4)
-    else:
-        message = f"Incorrect sound for {rat} not found"
-        print(message)
-        try:
-            telegram_bot.alarm_finish_session(message, rat)
-        except:
-            print("Telegram message not sent.")
-
+    soundStream.play(soundVec3)
 
     cam2.put_state("Punish")
     cam3.put_state("Punish")
